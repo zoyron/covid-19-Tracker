c5ae21a76452e8739cc0b403282fde7c9a5dbd4d
zoyron <sgrarora3@gmail.com>
2020-05-14 11:51:18 +0530

    fetched the required data from the api and added a little style to the App component. Will start working on the card component soon

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
-
-// following is a basic react class based component
+import styles from "./App.module.css";
+import { fetchData } from "./api/index";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
   render() {
+    const { data } = this.state;
     return (
-      <div>
+      <div className={styles.container}>
         <h1>This is COVID-19 Tracker project</h1>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
