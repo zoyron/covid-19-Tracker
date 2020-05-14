373a478a9e530f6808e10775724e84eef2b88af5
zoyron <sgrarora3@gmail.com>
2020-05-14 14:54:04 +0530

    fixed minor bugs of the chart and added the style as well

--- src/api/index.js
+++ src/api/index.js
@@ -28,6 +28,11 @@ export const fetchData = async () => {
 export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get(`${url}/daily`);
-    console.log(data);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportData,
+    }));
+    return modifiedData;
   } catch (error) {}
 };
