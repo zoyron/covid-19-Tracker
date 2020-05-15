b40f514572323111660c8b18bf9e135f76b97943
Shivam Bisht <shivam.bisht@thoughts2binary.com>
2020-05-15 12:05:41 +0530

    added spinner, updated App.js to use hooks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import React from "react";
-import Cards from "./components/Cards/Cards";
-import Chart from "./components/Chart/Chart";
-import CountryPicker from "./components/CountryPicker/CountryPicker";
-import styles from "./App.module.css";
-import { fetchData } from "./api/index";
-import coronaImage from "./images/image.png";
+import React, { useState, useEffect } from 'react';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+import { fetchData } from './api/index';
+import coronaImage from './images/image.png';
+import Spinner from './components/Spinner';
 
-class App extends React.Component {
-  state = {
-    data: {},
-    country: "",
+const App = () => {
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const getData = async () => {
+      const fetchedData = await fetchData(country);
+      setData(fetchedData);
+      setLoading(false);
+    };
+    getData();
+  }, [country]);
+
+  const handleCountryChange = (e) => {
+    setCountry(e.target.value);
   };
-  async componentDidMount() {
-    const fetchedData = await fetchData();
-    this.setState({ data: fetchedData });
-  }
 
-  handleCountryChange = async (country) => {
-    const fetchedData = await fetchData(country);
-    this.setState({ data: fetchedData, country: country });
-  };
-
-  render() {
-    const { data, country } = this.state;
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={coronaImage} alt="COVID-19 image" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.container}>
+      {loading && <Spinner />}
+      <img className={styles.image} src={coronaImage} alt="COVID-19 image" />
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart data={data} country={country} />
+    </div>
+  );
+};
 
 export default App;
